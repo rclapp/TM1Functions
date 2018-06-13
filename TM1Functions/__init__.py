860f2be3b456c2fcdf55956079bdb38b53f72d52
rclapp <rdclapp@amazon.com>
2018-06-13 10:15:18 -0700

    Create __init__.py
    New Init file

---
+++ TM1Functions/__init__.py
@@ -0,0 +1,10 @@
+"""
+A python module for added TM1 Functionality
+
+https://github.com/rclapp/TM1Functions
+
+""" 
+
+# __init__ can hoist attributes from submodules into higher namespaces for convenience
+
+from TM1Functions import TM1Functions
